37864b8297f1ef9d406604ec6e7d02b09f22b445
shelynna <sheilamakynhills@gmail.com>
2023-01-23 21:19:29 -0800

    update

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 def safe_print_list(my_list=[], x=0):
     """Print x elememts of a list.
 
@@ -19,4 +18,3 @@ def safe_print_list(my_list=[], x=0):
             break
     print("")
     return (ret)
-
--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,9 +1,5 @@
-
 #!/usr/bin/python3
-
 import sys
-
-
 def safe_print_integer_err(value):
     """Prints an integer with "{:d}".format().
 
